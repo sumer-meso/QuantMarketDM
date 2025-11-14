b4628f148c08d403b04a0202c35a96ae29e35ece
Ben MesoAir <ben.sumer.meso@gmail.com>
2025-11-14 14:51:04 +0800

    [config] simplized rabbitmq config and add http port

--- common/config.go
+++ common/config.go
@@ -5,6 +5,7 @@ import "encoding/json"
 type RabbitMQ struct {
 	Host     string `json:"host"`
 	Port     string `json:"port"`
+	HttpPort string `json:"http_port"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
@@ -16,8 +17,7 @@ type IndicatorSettings struct {
 }
 
 type ConfigFile struct {
-	RabbitmqTest RabbitMQ                                `json:"rabbitmqTest"`
-	RabbitmqProd RabbitMQ                                `json:"rabbitmqProd"`
-	Sources      map[string]map[string]IndicatorSettings `json:"sources"`
-	Strategy     json.RawMessage                         `json:"strategy"`
+	Rabbitmq RabbitMQ                                `json:"rabbitmq"`
+	Sources  map[string]map[string]IndicatorSettings `json:"sources"`
+	Strategy json.RawMessage                         `json:"strategy"`
 }
