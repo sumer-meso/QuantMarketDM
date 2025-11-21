8c268230ad8139396e8debcd80a6536157646b62
Ben MesoAir <ben.sumer.meso@gmail.com>
2025-11-21 23:10:33 +0800

    [OnHandler] pass in pointer.

--- tide/datasink.go
+++ tide/datasink.go
@@ -10,15 +10,15 @@ import (
 // DataSink receives decoded data from wire.
 // Implement only the methods you need; others may be no-ops.
 type DefaultDataSink interface {
-	OnUnknown(ctx context.Context, u proto.Unknown) error
+	OnUnknown(ctx context.Context, u *proto.Unknown) error
 }
 
 // For binance data sink, implement BNDataSink instead.
 type BNDataSink interface {
-	OnBNTrade(ctx context.Context, t binance.Trade) error
-	OnBNKline(ctx context.Context, k binance.Kline) error
-	OnBNOrderBook(ctx context.Context, ob binance.OrderBook) error
-	OnBNTradeLite(ctx context.Context, ob binance.TradeLite) error
-	OnBNAccountUpdate(ctx context.Context, ob binance.AccountUpdate) error
-	OnBNOrderUpdate(ctx context.Context, ob binance.OrderUpdate) error
+	OnBNTrade(ctx context.Context, t *binance.Trade) error
+	OnBNKline(ctx context.Context, k *binance.Kline) error
+	OnBNOrderBook(ctx context.Context, ob *binance.OrderBook) error
+	OnBNTradeLite(ctx context.Context, ob *binance.TradeLite) error
+	OnBNAccountUpdate(ctx context.Context, ob *binance.AccountUpdate) error
+	OnBNOrderUpdate(ctx context.Context, ob *binance.OrderUpdate) error
 }
