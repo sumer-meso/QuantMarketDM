57a89a73a9a84aa1ef706f0d82f02fe3d265ce91
Ben MesoAir <ben.sumer.meso@gmail.com>
2025-11-26 16:35:51 +0800

    [kline] kline range to details.

--- proto/binance/kline.go
+++ proto/binance/kline.go
@@ -18,18 +18,33 @@ type TrueRangeRatio struct {
 	Value        float64
 }
 
+type KlineLite struct {
+	StartTime int64
+	EndTime   int64
+	Open      float64
+	Close     float64
+	High      float64
+	Low       float64
+	Volume    float64
+}
+
+type KlineDetails struct {
+	Kline           KlineLite
+	TrueRangeRatios TrueRangeRatio
+	Volumes         float64
+}
+
 // This is Kline model which is used to send out.
 type Kline struct {
-	Symbol          string
-	TrueRangeRatios map[string]TrueRangeRatio
-	Volumes         map[string]float64
-	LocalTime       int64
-	Source          string
+	Symbol    string
+	Range     map[string]KlineDetails
+	LocalTime int64
+	Source    string
 }
 
 func (k *Kline) String() string {
-	return fmt.Sprintf("Kline (%s), t:%v, v:%v, s:%s, l:%d",
-		k.Symbol, k.TrueRangeRatios, k.Volumes, k.Source, k.LocalTime)
+	return fmt.Sprintf("Kline (%s), r:%v, s:%s, l:%d",
+		k.Symbol, k.Range, k.Source, k.LocalTime)
 }
 
 func (k *Kline) RMQRoutingIdentifier() string {
@@ -70,32 +85,4 @@ func (k *Kline) RMQDecodeMessage(m *proto.MessageOverRabbitMQ) error {
 		return proto.NotMatchError{Expected: k.RMQDataIdentifier(), Actual: m.DataIdentifier}
 	}
 	return json.Unmarshal(m.Body, k)
-}
-
-// WsKlineEvent define websocket kline event
-type WsKlineEvent struct {
-	Event  string  `json:"e"`
-	Time   int64   `json:"E"`
-	Symbol string  `json:"s"`
-	Kline  WsKline `json:"k"`
-}
-
-// WsKline define websocket kline
-type WsKline struct {
-	StartTime            int64  `json:"t"`
-	EndTime              int64  `json:"T"`
-	Symbol               string `json:"s"`
-	Interval             string `json:"i"`
-	FirstTradeID         int64  `json:"f"`
-	LastTradeID          int64  `json:"L"`
-	Open                 string `json:"o"`
-	Close                string `json:"c"`
-	High                 string `json:"h"`
-	Low                  string `json:"l"`
-	Volume               string `json:"v"`
-	TradeNum             int64  `json:"n"`
-	IsFinal              bool   `json:"x"`
-	QuoteVolume          string `json:"q"`
-	ActiveBuyVolume      string `json:"V"`
-	ActiveBuyQuoteVolume string `json:"Q"`
 }
