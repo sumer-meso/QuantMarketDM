175d0cc41b7011de3ce4ebaddee0d7421ac4bf52
Ben MesoAir <ben.sumer.meso@gmail.com>
2025-11-27 19:17:33 +0800

    [config] new config file model with indicator and accounts inside source.

--- common/config.go
+++ common/config.go
@@ -10,14 +10,24 @@ type RabbitMQ struct {
 	Password string `json:"password"`
 }
 
+type AccountSettings struct {
+	AccountID string `json:"account_id,omitempty"`
+	ApiKey    string `json:"api_key,omitempty"`
+	SecretKey string `json:"secret_key,omitempty"`
+}
+
 type IndicatorSettings struct {
 	Spread     string `json:"spread,omitempty"`
 	Imbalances string `json:"imbalances,omitempty"`
-	SecretKey  string `json:"secretkey,omitempty"`
+}
+
+type SourceSettings struct {
+	Accounts   []AccountSettings            `json:"accounts,omitempty"`
+	Indicators map[string]IndicatorSettings `json:"indicators,omitempty"`
 }
 
 type ConfigFile struct {
-	Rabbitmq RabbitMQ                                `json:"rabbitmq,omitempty"`
-	Sources  map[string]map[string]IndicatorSettings `json:"sources,omitempty"`
-	Strategy json.RawMessage                         `json:"strategy,omitempty"`
+	Rabbitmq RabbitMQ                  `json:"rabbitmq"`
+	Sources  map[string]SourceSettings `json:"sources"`
+	Strategy json.RawMessage           `json:"strategy,omitempty"`
 }
